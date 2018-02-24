d51595825aa5f4daccdd11725e89bfe44b8b2dfd
Omer Khan <omerkhan842@gmail.com>
2018-02-24 09:23:14 +0500

    javascript 1st tasks

---
+++ app.js
@@ -0,0 +1,187 @@
+// //==============================================
+// //====================ALERT=====================
+// //==============================================
+
+// //=====================TASK 1========================
+
+// alert("Welcome to My Web Page");
+
+// //=====================TASK 2====================
+
+// alert("Error! Please enter a valid Password.");
+
+// //====================TASK 3=====================
+
+// alert("Welcome to JS land \n Happy coding!");
+
+// //==================TASK 4================
+
+
+
+// //==============================================
+// //====================VARIABLES FOR STRINGS=====================
+// //==============================================
+
+
+// //========================TASK 1=================
+
+// var userName;
+
+// //========================TASK 2=================
+
+// var myName = "Muhammad Omer Khan";
+
+// //========================TASK 3=================
+
+// var messege = "Hello world";
+// alert(messege);
+
+// //========================TASK 4=================
+
+// var name = "Omer Khan";
+// var age = "22 years old";
+// var uni = "UBIT";
+// alert(name + "\n" + age + "\n" + uni);
+
+// //========================TASK 5=================
+
+// alert(" Pizza \n Pizz \n piz \n pi \n p");
+
+// //========================TASK 6=================
+
+// var email = "omerkhan842@example.com";
+// alert("My email is " + email);
+
+// //========================TASK 7=================
+
+// document.write("<b>YAAY i can write HTML through javascript</b>");
+
+
+
+
+// //==============================================
+// //====================VARIABLES FOR NUMBERS=====================
+// //==============================================
+
+
+// //==================TASK 1====================
+
+// var age = 22;
+
+// alert("i am " +age + " years old");
+
+// //==================TASK 2====================
+
+// var visit = 21;
+// alert("you have visited " +visit+ "times");
+
+// //==================TASK 3====================
+
+// var birthYear = 1996;
+
+// document.write("My birth year is " +birthYear + "<br> Data type of my declare variable is Number");
+
+// //==================TASK 4====================
+
+// document.write("<br>Omer khan orderd 5 t-shirts from Daraz.pk");
+
+
+//==============================================
+//====================MATH EXPRESSIONS=====================
+//==============================================
+
+// // //==================TASK 1====================
+
+// var a=2;
+// var b=3;
+// var c= a+b;
+
+// alert("sum of "+a+" and "+b+" = "+c);
+
+// // //==================TASK 2====================
+
+// var d,e,f;
+// d=4;
+// e=2;
+// f=d/e;
+// alert("d/f = "+f);
+
+// f=d-e;
+// alert("d-e = " + f);
+
+// f=d*e;
+// alert("d*e = "+f);
+
+
+//==============================================
+//====================12-13_IF...ELSE_ELSE...IFStatements_SetOfConditions=====================
+//==============================================
+
+//==================TASK 1====================
+
+// var num = +prompt("enter a number to check");
+
+// num = num%3;
+
+// if(num == 0)
+// {
+//     alert("Given number is divisible by 3");
+
+// }
+// else
+// {
+//     alert("not divisible");
+
+// }
+
+
+//==================TASK 2====================
+
+// var num = +prompt("Enter the number to check");
+
+// num = num%2;
+
+// if(num==0)
+// {
+//     alert("Number is even");
+
+// }
+
+// else{
+
+//     alert("number is odd");
+// }
+
+//==================TASK 3====================
+
+// var age = +prompt("enter your age");
+
+// if(age>18)
+// {
+//     alert("your're too old");
+
+// }
+// else{
+
+//     alert("too young");
+// }
+
+//==================TASK 4====================
+
+// var name = "OMER";
+
+// var theirName = prompt("enter your name");
+// theirName= theirName.toUpperCase();
+
+// if(theirName==name)
+// {
+//     alert("Welcome to the page " +theirName)
+
+// }
+
+// else{
+
+// }
+
+
+//==================TASK 5====================
